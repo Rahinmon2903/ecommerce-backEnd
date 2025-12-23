9600d11e5656f2238101eed29e4cc42832b2d8de
Rahinmon2903 <rahin2903@gmail.com>
2025-12-23 10:40:08 +0530

    authController completed

---
+++ Controller/authController.js
@@ -0,0 +1,40 @@
+import User from "../Model/userSchema.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+//Register
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+//login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        res.status(200).json({message: "User logged in successfully",token:token}); 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
---
+++ Route/authRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import authController from "../Controller/authController.js";
+
+
+const router = express.Router();
+
+
+
+export default router;
