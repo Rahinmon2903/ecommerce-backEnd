50d6877d13e1dd1933e4ac3fb794076632eab62a
Rahinmon2903 <rahin2903@gmail.com>
2025-12-30 09:51:26 +0530

    product Schema competed

---
+++ Model/productSchema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    stock: {
+      type: Number,
+      default: 1
+    },
+    images: [
+      {
+        type: String
+      }
+    ],
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
