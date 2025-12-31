66dea9b158539ba2f876ff5aaeac024f5d3158b6
Rahinmon2903 <rahin2903@gmail.com>
2025-12-31 10:39:21 +0530

    product update completed

--- Controller/productController.js
+++ Controller/productController.js
@@ -25,3 +25,27 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//update
+export const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+
+        if(!product){
+            return res.status(404).json({message:"Product not found"})
+        }
+
+        if(product.seller.toString() !== req.user._id.toString()){
+           return res.status(403).json({message:"You are not authorized"})
+        }
+        const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+         res.status(200).json({message:"Product updated successfully", updated})
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        
+    }
+}
+
