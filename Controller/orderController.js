a0f2a2b045f3ed6bd64b9a9f01d41402d7a70f6e
Rahinmon2903 <rahin2903@gmail.com>
2026-01-02 12:02:17 +0530

    get my orders completed

--- Controller/orderController.js
+++ Controller/orderController.js
@@ -43,3 +43,18 @@ export const placeOrder=async(req,res)=>{
         
     }
 }
+
+//get my orders
+
+export const getMyOrders=async(req,res)=>{
+    try {
+        const orders =await Order.findOne({buyer:req.user._id}).populate("products.productId","name price");
+
+        res.status(200).json({message:"Orders found", orders})
+        
+    } catch (error) {
+        res.status(500).json({message:error.message})
+        
+    }
+}
+   
