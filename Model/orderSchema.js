6d90599d9310d00a32c4e2432cece86f468a963c
Rahinmon2903 <rahin2903@gmail.com>
2026-01-02 11:19:57 +0530

    orderSchema completed

---
+++ Model/orderSchema.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const orderSchema=new mongoose.Schema({
+    buyer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+        
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    amount: {
+        type: Number,
+        required: true
+    },
+
+    
+    status: {
+        type: String,
+        enum: ['pending','paid','shipped', 'delivered', 'cancelled'],
+        default: 'pending'
+    }
+
+},
+  { timestamps: true }
+);
+
+const Order=mongoose.model("Order",orderSchema);
+export default Order;
