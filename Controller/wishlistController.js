cb010fcea564817f5d4c135775bf46209a51844e
Rahinmon2903 <rahin2903@gmail.com>
2026-01-08 19:59:11 +0530

    comments updated in wishlistController

--- Controller/wishlistController.js
+++ Controller/wishlistController.js
@@ -2,10 +2,11 @@ import User from "../Model/userSchema.js";
 
 export const addToWishlist = async (req, res) => {
   try {
+    // Destructure req.body
     const { productId } = req.body;
-
+   // Find cart
     const user = await User.findById(req.user._id);
-
+   // 1 If user does not exist
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -20,7 +21,7 @@ export const addToWishlist = async (req, res) => {
         message: "Product already in wishlist",
       });
     }
-
+  // Add product to wishlist
     user.wishlist.push(productId);
     await user.save();
 
@@ -36,9 +37,10 @@ export const addToWishlist = async (req, res) => {
 
 export const getWishlist = async (req, res) => {
   try {
+    // Find wishlist in userSchema
     const user = await User.findById(req.user._id)
       .populate("wishlist");
-
+  //
     res.status(200).json({
       wishlist: user?.wishlist || [],
     });
@@ -49,18 +51,19 @@ export const getWishlist = async (req, res) => {
 
 export const removeFromWishlist = async (req, res) => {
   try {
+    // Destructure req.body
     const { productId } = req.params;
-
+  // Find user
     const user = await User.findById(req.user._id);
-
+// 1 If user does not exist
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
+// Remove product from wishlist by its productId
     user.wishlist = user.wishlist.filter(
       (id) => id.toString() !== productId
     );
-
+  // Save user
     await user.save();
 
     res.status(200).json({
