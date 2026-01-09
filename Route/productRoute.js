f85199b55c5f34c2f639f4d625ee86d393750857
Rahinmon2903 <rahin2903@gmail.com>
2026-01-09 20:00:52 +0530

    cloudinary and multer added

--- Route/productRoute.js
+++ Route/productRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { authMiddleware, buyerOnly, sellerOnly } from "../Middleware/authMiddleware.js";
 import { addReview, createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "../Controller/productController.js";
+import upload from "../Middleware/upload.js";
 
 
 const router = express.Router();
@@ -8,7 +9,7 @@ const router = express.Router();
 //seller routes
 
 
-router.post("/create",authMiddleware,sellerOnly,createProduct );
+router.post("/create",authMiddleware,sellerOnly,upload.array("images", 5),createProduct );
 router.put("/update/:id",authMiddleware,sellerOnly,updateProduct);
 router.delete("/delete/:id",authMiddleware,sellerOnly,deleteProduct);
 
